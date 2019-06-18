d8460530596bd17cee42a785ab3c5cc8942949f4
Vineet Kumar <44500552+heisenberg-official@users.noreply.github.com>
2019-06-19 01:27:59 +0530

    Update Screen-Off-Script.cpp

--- Screen-Off-Script.cpp
+++ Screen-Off-Script.cpp
@@ -1,9 +1,8 @@
 #include <Windows.h>
-
 int main()
 {
 	SendMessage(HWND_BROADCAST,WM_SYSCOMMAND,SC_MONITORPOWER, (LPARAM)2);
-	Sleep(5000);
+	Sleep(5000); \\ Sleep For 5 Seconds OR Tap Button To WakeUP
 	SendMessage(HWND_BROADCAST,WM_SYSCOMMAND,SC_MONITORPOWER, (LPARAM)-1);
 	return 0;
 }
