9985fbf2d13cbc7786c8c6595298ae9621ae88c0
Vineet Kumar <44500552+heisenberg-official@users.noreply.github.com>
2019-06-19 01:27:15 +0530

    Add files via upload

---
+++ Screen-Off-Script.cpp
@@ -0,0 +1,9 @@
+#include <Windows.h>
+
+int main()
+{
+	SendMessage(HWND_BROADCAST,WM_SYSCOMMAND,SC_MONITORPOWER, (LPARAM)2);
+	Sleep(5000);
+	SendMessage(HWND_BROADCAST,WM_SYSCOMMAND,SC_MONITORPOWER, (LPARAM)-1);
+	return 0;
+}
