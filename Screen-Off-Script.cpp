9b1a20d8328149e07e2331e7037d84dec4b36551
Vineet Kumar <44500552+heisenberg-official@users.noreply.github.com>
2019-06-19 01:31:16 +0530

    Update Screen-Off-Script.cpp

--- Screen-Off-Script.cpp
+++ Screen-Off-Script.cpp
@@ -2,7 +2,7 @@
 int main()
 {
 	SendMessage(HWND_BROADCAST,WM_SYSCOMMAND,SC_MONITORPOWER, (LPARAM)2);
-	Sleep(5000); \\ Sleep For 5 Seconds OR Tap Button To WakeUP
+	Sleep(5000); //Sleep For 5 Seconds OR Tap Button To WakeUP
 	SendMessage(HWND_BROADCAST,WM_SYSCOMMAND,SC_MONITORPOWER, (LPARAM)-1);
 	return 0;
 }
